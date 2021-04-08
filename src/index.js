205f7ec63ebb8039a6d277f4e0b7aa5831435b1b
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-08 01:50:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let todoLi = document.createElement("li");
-  let tasks = document.getElementById("tasks");
-  let todoSubmit = document.getElementById("submit");
-  let TodoInput = document.getElementById("new-task-description");
+
 
   
   function submit (){
+    let todoLi = document.createElement("li");
+    let tasks = document.getElementById("tasks");
+    let todoSubmit = document.getElementById("submit");
+    let TodoInput = document.getElementById("new-task-description");
     todoLi.innerHTML = TodoInput.value;
     tasks.append(todoLi);
   }
