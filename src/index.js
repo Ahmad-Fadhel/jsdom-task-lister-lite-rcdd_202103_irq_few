692bbf5a332b36e9818c9a999d3c74911dfb5d58
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-08 01:43:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  
+  let todoLi = document.createElement("li");
+  let todoSubmit = document.getElementById("");
+  todoLi.innerHTML = 
 });
