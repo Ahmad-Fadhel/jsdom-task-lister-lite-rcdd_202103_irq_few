170d7e141d95615f215d8542fc7dbc4e22ed3678
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-08 02:01:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
 const creatTask = document.getElementById("create-task-form");
-const newTaskDescription = document.getElementById("new-task-description");
+const creatTaskDescription = document.getElementById("new-task-description");
 const taskList = document.getElementById("tasks");
 
 creatTask.addEventListener("submit", createNewTask);
@@ -11,7 +11,7 @@ function createNewTask(event) {
     console.log (event)
     event.preventDefault();
     const newTask = document.createElement("li");
-    newTask.innerText = newTaskDescription.value;
+    newTask.innerText = creatTaskDescription.value;
     taskList.appendChild(newTask);
     event.target.reset();
   };
