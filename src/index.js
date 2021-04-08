a78ec57d9c73ba93cec22ccddcf8271c8bf13462
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-08 01:46:39 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let todoLi = document.createElement("li");
+  let tasks = document.getElementById("tasks");
   let todoSubmit = document.getElementById("submit");
   let TodoInput = document.getElementById("new-task-description");
   todoLi.innerHTML = TodoInput.value;
+  tasks.append(todoLi);
 });
